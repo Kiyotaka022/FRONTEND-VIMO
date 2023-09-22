02c7ef49889fb2026287098813edd3fb93599e85
Angel <luisangelacostagonzalez2@gmail.com>
2023-09-21 20:22:29 -0500

    Abonos update

--- src/components/Abonos.jsx
+++ src/components/Abonos.jsx
@@ -42,6 +42,13 @@ function Dialog({setAbonos, setDialog, setEdit, setError, edit}) {
         return fechaNacimiento
    }
 
+   function handleClose(){
+    setDialog(false)
+    if (edit[0]){
+        setEdit(false)
+    }
+}
+
     async function handleSubmit(event){
         event.preventDefault()
         const {id, idVentaPedido, valorAbonado}= Object.fromEntries(new FormData(event.currentTarget))
@@ -75,30 +82,30 @@ function Dialog({setAbonos, setDialog, setEdit, setError, edit}) {
     return (
       <>
       <dialog className="AMODAL container border-2 border-blue-700 rounded-2xl w-[650px] bg-Rwhite py-3 top-[10px]" open>
-      <button className='ml-auto mr-[10px] block'><FontAwesomeIcon className='fa-2xl' icon={faCircleXmark}  onClick={()=>(setDialog(false))} /></button>
+      <button className='ml-auto mr-[10px] block'><FontAwesomeIcon className='fa-2xl' icon={faCircleXmark}  onClick={handleClose} /></button>
                     <span className="block text-4xl font-sans font-semibold text-center mb-[30px]">Abono</span>
                     <form className=" bg-Rwhite rounded-2xl py-2 px-3 flex items-start flex-wrap content-start gap-y-4 gap-x-4 justify-center" onSubmit={handleSubmit}>
 
                     <div className=" inline-block w-[280px] border-2 hover:border-formInputs1   rounded-2xl py-1 px-2">
                             <label className="text-xs font-sans font-semibold ml-3">Id</label>
-                            <input required type="number" min="0" name='id' className="block w-[95%] text-1xl font-sans font-semibold px-3 w-3/4 h-10 py-2 outline-0" placeholder="1,352,2,4"></input>
+                            <input required type="number" min="0" name='id' value={edit[0]? edit[1].id:null} className="block w-[95%] text-1xl font-sans font-semibold px-3 w-3/4 h-10 py-2 outline-0" placeholder="1,352,2,4"></input>
                     </div>
 
                     <div className=" inline-block w-[280px] border-2 hover:border-formInputs1  rounded-2xl py-1 px-2">
                             <label className="text-xs font-sans font-semibold ml-3">valor a Abonar</label>
-                            <input required type="number" min="0" name='valorAbonado' className="block w-[95%] text-1xl font-sans font-semibold px-3 w-3/4 h-10 py-2 outline-0" placeholder="1,352,2,4"></input>
+                            <input required type="number" min="0" name='valorAbonado' defaultValue={edit[0]? edit[1].valorAbonado:null} className="block w-[95%] text-1xl font-sans font-semibold px-3 w-3/4 h-10 py-2 outline-0" placeholder="1,352,2,4"></input>
                     </div>
 
 
                     <div className=" inline-block w-[280px] border-2 hover:border-formInputs1 rounded-2xl py-1 px-2">
                             <label className="text-xs font-sans font-semibold ml-3">Código del Pedido</label>
-                            <input required type="number" min="0" name='idVentaPedido' className="block w-[95%] text-1xl font-sans font-semibold px-3 w-3/4 h-10 py-2 outline-0" placeholder="1,352,2,4"></input>
+                            <input required type="number" min="0" name='idVentaPedido'  defaultValue={edit[0]? edit[1].idVentaPedido:null} className="block w-[95%] text-1xl font-sans font-semibold px-3 w-3/4 h-10 py-2 outline-0" placeholder="1,352,2,4"></input>
                     </div>
                         <div className=" inline-block w-[280px] border-none rounded-2xl py-1 px-2">
                             <label className="text-xs font-sans font-semibold ml-3"></label>
                        </div>
                     
-                       <Button msg={"+Añadir"}></Button>
+                       <Button msg={edit[0]?"Editar":"+ Añadir"}></Button>
                        </form>
                 </dialog>
       </>
